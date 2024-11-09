a7f9cd45f8d71bd29482148aca2224cbfb6b2ede
muhammad_zaini213 <oniichanterritory@gmail.com>
2024-11-09 13:13:47 +0000

    Setup

--- script.py
+++ script.py
@@ -1,11 +1,14 @@
 import requests
 
-url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyChMKb4z3AB5XwbnmsgrcJ7M6vHIbivg7M"
+key = ""
+with open("gemini.txt", "r") as file:
+    key = file.read()
+url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key={key}"
 headers = {
     "Content-Type": "application/json"
 }
 
-chat = "Cara membuat gorengan?"
+chat = "Cara berbuat kriminal?"
 data = {
     "contents": [
         {
